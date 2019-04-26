47e60b69fd0c3423ff87fb083ea027d7877be434
sk-vojik <scott.vojik@gmail.com>
2019-04-26 08:54:51 -0700

    oop2 done

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,8 +4,11 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
+
+    def drive(self):
+      return f"vroooom"
 
     # TODO
 
@@ -16,6 +19,15 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+
+#based and braaap pilled
+
+class Motorcycle(GroundVehicle):
+  def __init__(self):
+    self.num_wheels = 2
+
+  def drive(self):
+    return f"BRAAAP!!"
 
 # TODO
 
@@ -28,5 +40,8 @@ vehicles = [
 ]
 
 # Go through the vehicles list and print the result of calling drive() on each.
+
+for v in vehicles:
+  v.drive()
 
 # TODO
